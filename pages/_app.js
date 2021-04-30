a71dcacdfbb74a66fec5bfa9fcbc64c6ecbd6df2
Becelli <gustavobecelli@gmail.com>
2021-04-30 15:05:27 -0300

    renaming project

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
 	return (
 		<section className="bg-black text-white">
+			<Head>
+				<title>FCT-OS 0.1-ALPHA</title>
+			</Head>
 			<Component {...pageProps} />
 		</section>
 	);
