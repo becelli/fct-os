2217db1ed7ff6de46718325d872fbc02d06716ee
Becelli <gustavobecelli@gmail.com>
2021-05-07 17:05:22 -0300

    correct timing

--- pages/setup/index.js
+++ pages/setup/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import Construction from '../../components/construction';
+// import Construction from '../../components/construction';
+const tVoid = 2.5;
+const tLogo = 6.5;
+const tPage = 9;
+
+import Quiz from '../../components/quiz';
 
 export default function setUp() {
 	const [show, setShow] = useState(0);
 	useEffect(() => {
 		setTimeout(() => {
 			if (show < 1) setShow(1);
-		}, 2500);
+		}, tVoid * 1000);
 		setTimeout(() => {
 			if (show < 2) setShow(2);
-		}, 5500);
+		}, tLogo * 1000);
 		setTimeout(() => {
 			if (show < 3) setShow(3);
-		}, 8500);
+		}, tPage * 1000);
 	}, [show]);
+	
 
 	if (!show || show == 2) return null;
 	if (show == 1)
@@ -24,10 +30,15 @@ export default function setUp() {
 			</main>
 		);
 	return (
-		<main className="h-screen w-screen flex">
-			<div className="m-auto">
-				<Construction />
-			</div>
+		<main className="h-screen w-screen hide-cursor">
+			<Quiz
+				op1="Descompactar dados"
+				op2="Verificar os terminais"
+				op3="Carregar tela de login"
+				op4="Carregar o sistema operacional"
+				correct="1"
+				link="http://google.com"
+			/>
 		</main>
 	);
 }
